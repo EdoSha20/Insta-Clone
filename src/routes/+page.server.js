b31ec989bb34d182dd2a3e9707c4dd7ed65c9577
Edona Shabaj <edosha20@htl-shkoder.com>
2026-06-03 10:17:44 +0200

    load images for homepage feed

---
+++ src/routes/+page.server.js
@@ -0,0 +1,24 @@
+import pool from '$lib/server/db.js';
+
+export async function load() {
+
+	const [images] = await pool.execute(`
+		SELECT
+			i.id,
+			i.image,
+			i.description,
+			i.votes,
+			i.created_at,
+			u.username AS author
+		FROM images i
+		INNER JOIN users u
+			ON i.author_id = u.id
+		ORDER BY i.votes DESC, i.created_at DESC
+		LIMIT 25
+	`);
+
+	return {
+		images
+	};
+
+}
